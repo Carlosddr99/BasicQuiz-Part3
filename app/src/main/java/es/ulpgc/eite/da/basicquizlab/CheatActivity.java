97b5abc53e3aad57a2ac40f7d8d788250497bac0
Carlos Daniel Díaz Rodríguez <carlosddr1999@hotmail.com>
2021-03-16 13:11:38 +0000

    End practicse 3

---
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/CheatActivity.java
@@ -0,0 +1,124 @@
+package es.ulpgc.eite.da.basicquizlab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheatActivity extends AppCompatActivity {
+
+  public static final String TAG = "Quiz.CheatActivity";
+
+  public final static String EXTRA_ANSWER = "EXTRA_ANSWER";
+  public final static String EXTRA_CHEATED = "EXTRA_CHEATED";
+  public final static String CHEATED_SHOW ="EXTRA_SHOW";
+
+
+  private Button noButton, yesButton;
+  private TextView answerText;
+
+  private int currentAnswer;
+  private boolean answerCheated;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_cheat);
+
+    getSupportActionBar().setTitle(R.string.cheat_title);
+
+    initLayoutData();
+
+    linkLayoutComponents();
+    if(savedInstanceState!=null){
+      answerCheated=savedInstanceState.getBoolean(EXTRA_CHEATED);
+      currentAnswer=savedInstanceState.getInt(CHEATED_SHOW);
+    }
+    enableLayoutButtons();
+  }
+
+  private void initLayoutData() {
+    currentAnswer = getIntent().getExtras().getInt(EXTRA_ANSWER);
+  }
+
+  private void linkLayoutComponents() {
+    noButton = findViewById(R.id.noButton);
+    yesButton = findViewById(R.id.yesButton);
+
+    answerText = findViewById(R.id.answerText);
+  }
+
+  private void enableLayoutButtons() {
+    if( answerCheated==false){
+      noButton.setOnClickListener(v -> onNoButtonClicked());
+      yesButton.setOnClickListener(v -> onYesButtonClicked());
+    }else{
+      yesButton.setEnabled(false);
+      noButton.setEnabled(false);
+
+      if(currentAnswer == 0) {
+        answerText.setText(R.string.false_text);
+      } else {
+        answerText.setText(R.string.true_text);
+
+      }
+
+    }
+
+  }
+
+  private void returnCheatedStatus() {
+    Log.d(TAG, "returnCheatedStatus()");
+    Log.d(TAG, "answerCheated: " + answerCheated);
+
+    Intent intent = new Intent();
+    intent.putExtra(EXTRA_CHEATED, answerCheated);
+
+
+    setResult(RESULT_OK, intent);
+
+    finish();
+  }
+
+  @Override
+  public void onBackPressed() {
+    Log.d(TAG, "onBackPressed()");
+
+    returnCheatedStatus();
+  }
+
+
+  private void onYesButtonClicked() {
+    yesButton.setEnabled(false);
+    noButton.setEnabled(false);
+    answerCheated = true;
+
+    if(currentAnswer == 0) {
+      answerText.setText(R.string.false_text);
+    } else {
+      answerText.setText(R.string.true_text);
+
+    }
+  }
+
+  private void onNoButtonClicked() {
+    yesButton.setEnabled(false);
+    noButton.setEnabled(false);
+
+    returnCheatedStatus();
+  }
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+   //  outState.putBoolean(CHEATED_SHOW,answerCheated);
+     outState.putBoolean(EXTRA_CHEATED,answerCheated);
+     outState.putInt(CHEATED_SHOW,currentAnswer);
+
+  }
+
+}
